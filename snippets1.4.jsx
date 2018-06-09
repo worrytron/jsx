fd98f624df88b28fb70058f5fbab3b788dd51b81
worrytron <rastermancer@gmail.com>
2018-06-09 15:59:00 -0400

    1.0.4 - XMPMeta extension work in progress

--- snippets1.4.jsx
+++ snippets1.4.jsx
@@ -192,6 +192,47 @@ function switchDashboardTextLayers (type) {
 
 
 
+XMPMeta.prototype.setDublinCoreData = function () {
+    var xmp = this;
+    xmp.appendArrayItem(XMPConst.NS_DC, "dc:publisher", "ESPN C&SD", 0, XMPConst.ARRAY_IS_ALTERNATIVE);
+    //xmp.setProperty(XMPConst.NS_DC, "dc:author", "Mark Rohrer");
+    //xmp.setProperty(XMPConst.NS_DC, "dc:copyright", "Copyright 2018 ESPN Productions Inc. All rights reserved.");
+}
+
+XMPMeta.prototype.getCustomData = function (field) {
+    var xmp = this;
+    if (!xmp.doesPropertyExist(XMPConst.NS_XMP, "ESPN"))
+        return false;
+    var value = xmp.getStructField(XMPConst.NS_XMP, "ESPN", XMPConst.TYPE_TEXT, field).value;
+    return value;
+}
+
+XMPMeta.prototype.setCustomData = function (field, value) {
+    var xmp = this;
+    if (!xmp.doesPropertyExist(XMPConst.NS_XMP, "ESPN"))
+        xmp.setProperty(XMPConst.NS_XMP, "ESPN", "", XMPConst.PROP_IS_STRUCT);
+    xmp.setStructField(XMPConst.NS_XMP, "ESPN", XMPConst.TYPE_TEXT, field, value);
+}
+
+var xmp;
+var proj = app.project;
+
+if(ExternalObject.AdobeXMPScript == undefined)
+    ExternalObject.AdobeXMPScript = new ExternalObject('lib:AdobeXMPScript');
+xmp = new XMPMeta(proj.xmpPacket);        
+
+xmp.setDublinCoreData();
+xmp.setCustomData("Production", "MFFFLB");
+xmp.setCustomData("Project", "MLkkkB_X_TEST");
+xmp.setCustomData("Scene", "TESkkkkT2");
+xmp.setCustomData("Version", "00kkkkkk02");
+
+proj.xmpPacket = xmp.serialize();
+
+$.writeln(xmp.serialize());
+
+
+
 
 
 
